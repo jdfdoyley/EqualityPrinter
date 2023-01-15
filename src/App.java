ab78804b2f48ea8dc7558615db6db6a08bff49ec
Jason D'Oyley <jdfdoyley@gmail.com>
2023-01-14 22:33:13 -0500

    Set up project start file

---
+++ src/App.java
@@ -0,0 +1,12 @@
+/**
+ * Author: Jason D'Oyley
+ * Date: Jan 14, 2023
+ * Section: 5 - Expressions, Statements & More
+ * Topic: Coding Exercise 11: Equality Printer
+ */
+
+public class App {
+    public static void main (String[] args) {
+
+    }
+}
