7229918158bb8619ecaac57bde8509c1c0523d02
Jason D'Oyley <jdfdoyley@gmail.com>
2023-01-14 22:45:58 -0500

    Code to check and print equality

--- src/App.java
+++ src/App.java
@@ -7,6 +7,20 @@
 
 public class App {
     public static void main (String[] args) {
+        printEqual(1, 1, 1);        // Prints: All numbers are equal
+        printEqual(1, 1, 2);        // Prints: Neither all are equal or different
+        printEqual(-1, -1, -1);     // Prints: Invalid Value
+        printEqual(1, 2, 3);        // All numbers different
+    }
 
+    public static void printEqual(int a, int b, int c) {
+        if (a < 0 || b < 0 || c < 0)
+            System.out.println("Invalid Value");
+        else if (a == b && b == c)
+            System.out.println("All numbers are equal");
+        else if (a != b && b != c && a != c)
+            System.out.println("All numbers are different");
+        else
+            System.out.println("Neither all are equal or different");
     }
 }
